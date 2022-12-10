4d2a8d13ae8c4503534a0f4210fbc967abcf1803
Dmitri-Krivov <dk4020788@gmail.com>
2022-12-10 15:00:22 +0200

    First commit

---
+++ src/main/java/telran/java2022/KarafkaDataConsumerMinApplication.java
@@ -0,0 +1,13 @@
+package telran.java2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarafkaDataConsumerMinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarafkaDataConsumerMinApplication.class, args);
+	}
+
+}
---
+++ src/main/java/telran/java2022/karafka/service/KarafkaService.java
@@ -0,0 +1,27 @@
+package telran.java2022.karafka.service;
+
+import java.util.function.Consumer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+import telran.java2022.karafka.dto.PulseDto;
+
+@Service
+public class KarafkaService {
+	
+	@Bean
+	public Consumer<PulseDto> receiveData(){
+		return data -> {
+//			try {
+//				Thread.sleep(3000);
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+			Long delay = System.currentTimeMillis() - data.getTimestamp();
+			System.out.println("delay: " + delay + ", id: " + data.getId() + ", pulse: " + data.getData());
+		};
+	}
+
+}
